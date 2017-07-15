e9f5bd31911452c3a5404d371df451d753da4e1e
Edward Sumerfield <esumerfield@C02Q7KGZG8WP.local>
2017-07-15 09:37:31 -0400

    make conventions clearer

--- sockets/echo_server.exs
+++ sockets/echo_server.exs
@@ -1,99 +1,114 @@
+#
+# A simple Echo Server to explor program structure.
+#
+# usage:
+#   elixir echo_server.exs
+#
+# clients:
+#   telnet localhost 2345
+#
+
+# Separate module to isolate program startup.
 defmodule Echo.Main do
-  def run do
+  def start do
     Echo.Control.control()
   end
 end
 
+# Module for the main erlang process that controls all linked child erlang processes
 defmodule Echo.Control do
-
   def control() do
     control_pid = self()
     spawn_monitor(Echo.Listen, :start, [control_pid])
-    monitor()
+    wait_for_command()
   end
 
-  def monitor() do
+  def wait_for_command() do
     receive do
-      _ -> IO.puts "Exiting..."
+      "shutdown"   -> IO.puts "Shutdown..."
+      message      -> IO.puts "Unexpected Exit: #{inspect message}"
     end
   end
 end
 
+# Module to isolate the first child process responsible for accepting connections.
 defmodule Echo.Listen do
+  # Convension all erlang processes have "start" function.
   def start(control_pid) do
     listening(control_pid)
   end
 
   def listening(control_pid) do
     IO.puts "Welcome to Echo"
-    listening(control_pid, :gen_tcp.listen(2345, 
-              [:binary, packet: :line, active: false, reuseaddr: true]))
+    listening(control_pid, 
+              :gen_tcp.listen(2345, [:binary, packet: :line, active: false, reuseaddr: true]))
   end
 
-  def listening(control_pid, {:ok, socket}) do
-    accept(control_pid, socket)
+  def listening(control_pid, {:ok, listen_socket}) do
+    accept_loop(control_pid, listen_socket)
   end
 
-  def listening(_, {:error, something}) do
-    IO.puts "LISTEN ERROR #{something}"
+  def listening(_, {:error, message}) do
+    IO.puts "LISTEN ERROR #{message}"
   end
 
-  def accept(control_pid, socket) do
-    accepted(control_pid, :gen_tcp.accept(socket))
-    accept(control_pid, socket)
+  # Convension to suffix _loop for control loops
+  def accept_loop(control_pid, listen_socket) do
+    accepted(control_pid, :gen_tcp.accept(listen_socket))
+    accept_loop(control_pid, listen_socket)
   end
 
-  def accepted(control_pid, {:ok, client}) do
-    spawn_monitor(Echo.Connection, :start, [control_pid, client])
+  def accepted(control_pid, {:ok, client_socket}) do
+    spawn_monitor(Echo.Connection, :start, [control_pid, client_socket])
   end
 
-  def accepted(_, {:error, something}) do
-    IO.puts "ACCEPT ERROR #{something}"
+  def accepted(_, {:error, message}) do
+    IO.puts "ACCEPT ERROR #{message}"
   end
 end
 
+# Module to isolate read/write of each connection
 defmodule Echo.Connection do
-  def start(control_pid, client) do
-    welcome(control_pid, client)
+  # Convension all erlang processes have "start" function.
+  def start(control_pid, client_socket) do
+    welcome(control_pid, client_socket)
   end
 
-  def welcome(control_pid, client) do
-    :gen_tcp.send(client, "Welcome (quit to exit)\n")
-    received_loop(control_pid, client)
+  def welcome(control_pid, client_socket) do
+    :gen_tcp.send(client_socket, "Welcome (quit to exit, or shutdown to stop everything)\n")
+    received_loop(control_pid, client_socket)
   end
 
-  def received_loop(control_pid, client) do
-    received_data(control_pid, client, :gen_tcp.recv(client, 0))
+  # Convension to suffix _loop for control loops
+  def received_loop(control_pid, client_socket) do
+    received_data(control_pid, client_socket, 
+                  :gen_tcp.recv(client_socket, 0))
   end
 
-  def received_data(_, client, {:ok, "quit\r\n"}) do
-    :gen_tcp.send(client, "Goodbye\n")
-    :gen_tcp.close(client)
-    IO.puts "QUIT #{inspect client}"
+  def received_data(_, client_socket, {:ok, "quit\r\n"}) do
+    :gen_tcp.send(client_socket, "Goodbye\n")
+    :gen_tcp.close(client_socket)
+    IO.puts "QUIT #{inspect client_socket}"
   end
 
-  def received_data(control_pid, client, {:ok, "shutdown\r\n"}) do
-    :gen_tcp.send(client, "Shutting Down\n")
-    :gen_tcp.close(client)
-    IO.puts "Shutting Down #{inspect client}"
+  def received_data(control_pid, client_socket, {:ok, "shutdown\r\n"}) do
+    :gen_tcp.send(client_socket, "Shutting Down\n")
+    :gen_tcp.close(client_socket)
+    IO.puts "Shutting Down #{inspect client_socket}"
     send control_pid, "shutdown"
   end
 
-  def received_data(control_pid, client, {:ok, data}) do
+  def received_data(control_pid, client_socket, {:ok, data}) do
     IO.puts "DATA #{inspect data}"
-    :gen_tcp.send(client, data)
-    received_loop(control_pid, client)
+    :gen_tcp.send(client_socket, data)
+    received_loop(control_pid, client_socket)
   end
 
-  def received_data(_, client, {:error, :closed}) do
-    :gen_tcp.close(client)
-    IO.puts "CLOSED #{inspect client}"
-  end
-
-  def close(socket) do
-    :gen_tcp.close(socket)
+  def received_data(_, client_socket, {:error, :closed}) do
+    :gen_tcp.close(client_socket)
+    IO.puts "CLOSED #{inspect client_socket}"
   end
 end
 
-Echo.Main.run
+Echo.Main.start
 
